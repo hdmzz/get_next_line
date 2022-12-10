9bd9772514ffc73e036e2e7c7fff333f6fa66619
Hugo Damitzian <hdamitzi@student.42lyon.fr>
2022-12-11 00:05:23 +0100

    norminette

--- get_next_line.h
+++ get_next_line.h
@@ -1,9 +1,11 @@
 #ifndef GET_NEXT_LINE_H
+
 # define GET_NEXT_LINE_H
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
 
 # define BUFFER_SIZE 10
 
